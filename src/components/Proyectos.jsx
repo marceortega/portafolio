5d1fc2cb714fbfc927173aac0965b08d3727cedc
marceortega <marcela.ortegao@mayor.cl>
2020-12-07 00:13:43 -0300

    proyectos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -3,10 +3,51 @@ import {
     Link
   } from "react-router-dom";
   import '../styles/Proyectos.css';
+  import Group13 from '../assets/img/Group13.svg'
+  import navbar from '../assets/img/navbar.svg'
 const Proyectos = () => {
     return (
         <div>
-            <div className="container">
+            <div className="proyect">
+            <div className="global-header">
+                
+                <div className="nombreLogo" data-aos="slide-left"
+                data-aos-duration="1000">  
+                
+                <Link to="/">
+                <img className="logoNombre" src={Group13} alt="" />
+             
+               </Link>
+                </div>
+            </div> 
+
+            <div className="global-nav">
+                
+                <div className="navEgg_svg" data-aos="slide-left"
+            data-aos-duration="1000">
+                    
+                   
+                    <img className="navbarImagen" src={navbar} alt="" />
+                 
+                   
+                    </div>
+                </div>
+                <div className="global-lines">
+                
+                <div className="linesLateralProyect" data-aos="fade-left"data-aos-duration="2000"> </div>
+
+                 <div className="linesLateralDosProyect" data-aos="fade-left"data-aos-duration="1000"> </div>
+          <div className="linesLateralTresProyect" data-aos="fade-left"
+               
+               data-aos-duration="1000">
+              
+          </div>
+</div>
+
+
+
+
+                
             <Link to="/"className="btn btn-dark">
                    Inicio
                </Link>
